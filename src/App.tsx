11e520405fb201bb4e36195d0073df109b791b67
Junior Ferreira <47725788+joseiltonjunior@users.noreply.github.com>
2021-05-09 15:47:17 -0300

    news implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { ThemeProvider } from "styled-components";
 
-function App() {
-  return <div className="App"></div>;
-}
+import Header from "./components/Header";
+import GlobalStyle from "./styles/global";
+
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme.title === "light" ? dark : light);
+  }, [theme.title]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
